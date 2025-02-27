3690919920731928e62b49e4818c77e2597200c1
VilchisV <vidosito1@gmail.com>
2025-02-27 08:06:22 -0600

    js

---
+++ login.js
@@ -0,0 +1,19 @@
+async function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    let data = await response.json();
+    document.getElementById("message").innerText = data.message;
+
+    if (response.ok) {
+        alert("Bienvenido, " + username);
+    } else {
+        alert("Error: " + data.message);
+    }
+}
