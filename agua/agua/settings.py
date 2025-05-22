296436b572ba0bc83aa112ad7003285ee09a0850
Alexis Salgado <alexissalgado512@gmail.com>
2025-05-22 13:30:10 -0600

    aaaaaa

--- agua/agua/settings.py
+++ agua/agua/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
